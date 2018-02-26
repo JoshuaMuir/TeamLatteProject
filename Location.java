a17def86b6ffddffc3c93e155371d33b19160a87
jcth37 <36866540+jcth37@users.noreply.github.com>
2018-02-26 15:03:30 -0800

    Add files via upload

---
+++ Location.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.unbc.cpsc.latte;//ask about interfaces package?
+
+/**
+ *
+ * @author jho
+ */
+public interface Location {
+    public static final int MAX_ROW = 3;
+    public static final int MIN_ROW = 0;
+    public static final int MAX_COLUMN = 3;
+    public static final int MIN_COLUMN = 0;
+    
+    public abstract int getrow();
+    public abstract int getColumn();
+}
