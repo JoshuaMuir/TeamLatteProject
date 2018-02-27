d3d40142b641f8459702de62f8b047bddb64b79f
jnnersli <36866593+jnnersli@users.noreply.github.com>
2018-02-26 20:26:42 -0800

    Update AI.java

--- AI.java
+++ AI.java
@@ -15,9 +15,11 @@ import ca.unbc.cpsc.score4.interfaces.Colour;
 import ca.unbc.cpsc.score4.interfaces.Location;
 import ca.unbc.cpsc.score4.exceptions.PlayerException;
 import ca.unbc.cpsc.latte.Referee;
+import java.util.Random;
 
 public class AI implements ca.unbc.cpsc.score4.interfaces.Player {
     private Colour colour;
+    private GameOverStatus gameOverStatus;
     
     public AI() {
         reset();
@@ -26,6 +28,8 @@ public class AI implements ca.unbc.cpsc.score4.interfaces.Player {
     @Override
     public void reset() throws PlayerException {
         startGameAs(Referee.assignColour());
+        gameOverStatus = null;
+        //or whatever method from referee class assigns colours
     }
     
     @Override
@@ -35,19 +39,19 @@ public class AI implements ca.unbc.cpsc.score4.interfaces.Player {
     
     @Override
     public void noteOpponentsId(int id) throws PlayerException {
-        
+        //dunno if necessary
     }
     
     @Override
     public void opponentPlays(Location ell) throws PlayerException {
-        
+        //do i even do anything for this
     }
     
     @Override
     public Location requestMoveLocation() throws PlayerException {
         Location move;
         return move;
-        /*pick a peg at random and check if it's empty? possibly with a 
+        /*pick a peg at random and check if it's full? possibly with a 
         static checkPeg method?*/
     }
     
@@ -57,6 +61,6 @@ public class AI implements ca.unbc.cpsc.score4.interfaces.Player {
     }
     
     public abstract void noteGameOver(GameOverStatus whatHappened) throws PlayerException {
-        
+        gameOverStatus = whatHappened;
     }
 }
