9cff340b578975d3f6e904ee1866b71b408422d6
RyanSilvestre <35552251+RyanSilvestre@users.noreply.github.com>
2018-02-28 02:11:10 -0800

    Add files via upload
    Referee

---
+++ Referee.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.unbc.cpsc.latte;
+import ca.unbc.cpsc.latte.Loc3d;
+import ca.unbc.cpsc.latte.Loc2d;
+import ca.unbc.cpsc.latte.Bead;
+import ca.unbc.cpsc.latte.player;
+import ca.unbc.cpsc.latte.AI;
+import ca.unbc.cpsc.latte.rules;//not sure if we are making this as a class or not.
+/**
+ *
+ * @author ryans
+ */
+public class Referee {
+    public void reset()
+    {
+    //restarts the game or starts it
+    }
+    public void placeBead()
+    {
+    //communicates with the bead to the peg
+    //isnt this the bead or player class job?
+    //checkmove
+    }
+    public void displayBoard()
+    {
+    //displays the board
+    }
+    public void checkMove(Location l)
+    {
+    //communicates with loc3d and loc2d
+    //maybe the rules to check
+    //checks if its a valid move
+    }
+    public void checkWin(Location l)
+    {
+    //communicates with loc3d and loc2d
+    //maybe the rules to check
+    //notify the players who won
+    }
+    public void assignColour()//or tracks color
+    {
+    //who the black or white is
+    }
+    public void rules()
+    {
+    //if we are not doing a rules class
+   
+    }
+   
+}
