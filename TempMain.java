94fc7086af9f194ee62e7cbd03967d0820f8e408
jcth37 <36866540+jcth37@users.noreply.github.com>
2018-03-09 16:53:49 -0800

    Add files via upload

---
+++ TempMain.java
@@ -0,0 +1,35 @@
+package ca.unbc.cpsc.latte;
+
+public class TempMain {
+    //start of game
+    //new board();
+
+    //we are given a move that the a player has made and need to put it on the board
+    //new bead b(color, location, board.getPeg(location).getLength());
+    //board.getPeg(location).addbead(0,b); , where 0 is unimportant
+    //board.addtoLines(b);
+    //done
+    
+    //referee is requesting a move; now what?
+    //ai chooses location and gives to referee
+    
+    //reset by getting a new board
+    
+    /* 
+    To do:
+    bead - color turn counter problem, color stuff
+    line - return game over somehow, and request different move
+    
+    */
+    /**
+     *
+     *
+     */
+    public static void main(String[] args) {
+        Board myBoard = new Board();
+        
+        Loc2d pass = new Loc2d(1,1);
+        
+
+    }
+}
