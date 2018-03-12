8551027bdc8b3b040cdc2224735c6b9e350fe6cb
jcth37 <36866540+jcth37@users.noreply.github.com>
2018-03-11 18:33:40 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -15,6 +15,8 @@ import ca.unbc.cpsc.score4.exceptions.PlayerException;
 public interface Player {
     public abstract void reset()throws PlayerException;
     
+    public abstract void noteOpponentsId(int id)throws PlayerException;
+    
     public abstract void startGameAs(Colour c) throws PlayerException;
     
     public abstract void opponentPlays(Location ell) throws PlayerException;
