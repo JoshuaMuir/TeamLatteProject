a59f42434bdc972dda77d27ab1aa5ed016ebd6ad
jcth37 <36866540+jcth37@users.noreply.github.com>
2018-03-12 14:45:44 -0700

    Add files via upload

--- AI.java
+++ AI.java
@@ -1,56 +1,40 @@
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package ca.unbc.cpsc.latte;
 
-/**
- *
- * @author jli3
- */
 import ca.unbc.cpsc.score4.enums.GameOverStatus;
 import ca.unbc.cpsc.score4.interfaces.Colour;
 import ca.unbc.cpsc.score4.interfaces.Location;
 import ca.unbc.cpsc.score4.exceptions.PlayerException;
-import ca.unbc.cpsc.latte.Referee;
 import java.util.Random;
 
 public class AI implements ca.unbc.cpsc.score4.interfaces.Player {
-    private PlayerColour colour;
+    private Colour colour;
     private GameOverStatus gameOverStatus;
     private int opponent;
-    //this is also not necessary and not used for anything but i'll just keep it anyway in case Casperson wants to do something
-    //in the tournament with it
     private int turnCount;
-    //turnCount is not necessary it's just a counter for my AI's strategy etc.
+    private Board board = new Board();
+    private Loc3d lastmove;
     
-    public AI() {
+    public AI() throws PlayerException{
         reset();
     }
     
+    public Board getBoard(){
+        return board;
+    }
+    
+    public Colour getColour(){
+        return colour;
+}
+    
     @Override
     public void reset() throws PlayerException {
-        startGameAs(Referee.assignColour());
-        //not sure if AI calls the referee method or if the referee calls AI.startGameAs directly
-        //also not sure what this method will be called
         gameOverStatus = null;
-        opponent = 0;
         turnCount = 1;
-        
-        /*
-        one BIG question I have is whether Referee calls all the methods like startGameAs(), noteOpponentsId(), or if
-        the referee just calls reset() and those methods get called by the AI's reset() method. like for example should
-        I call those methods in this method or does the Referee do that for me? if I am calling those methods I will need 
-        to receive information from the Referee that the Referee receives from Player etc. Very complicated, I know.
-        */
-        
+        board = new Board();
     }
     
     @Override
-    public void startGameAs(PlayerColour c) throws PlayerException {
+    public void startGameAs(Colour c) throws PlayerException {
         colour = c;
     }
     
@@ -66,14 +50,13 @@ public class AI implements ca.unbc.cpsc.score4.interfaces.Player {
     
     @Override
     public Loc3d requestMoveLocation() throws PlayerException {
-        Loc3d move;
+        Loc3d move = new Loc3d(0,0,0); //to ensure that move is initialized
         //for choosing a peg at random
         
         int row;
         int column;
         int height;
         
-        boolean sameColor;
         //for checking vertical lines of 3
         
         Bead b1;
@@ -81,34 +64,34 @@ public class AI implements ca.unbc.cpsc.score4.interfaces.Player {
         Bead b3;
         
         if(isFirstFour()) { //try to get a corner peg
-            if(Board.getPeg[0].getLength() == 0) {
+            if(board.getPeg(new Loc3d(3,0,0)).getLength() == 0) {
                  move = new Loc3d(3, 0, 0);
             }
-            else if(Board.getPeg[3].getLength() == 0) {
+            else if(board.getPeg(new Loc3d(3,3,0)).getLength() == 0) {
                  move = new Loc3d(3, 3, 0);
             }
-            else if(Board.getPeg[12].getLength() == 0) {
+            else if(board.getPeg(new Loc3d(0,0,0)).getLength() == 0) {
                  move = new Loc3d(0, 0, 0);
             }
-            else if(Board.getPeg[15].getLength() == 0) {
+            else if(board.getPeg(new Loc3d(0,3,0)).getLength() == 0) {
                  move = new Loc3d(0, 3, 0);
             }
         }
         else {
-            for(int i = 0; i < Board.getPeg[].length; i++) {
-                if(Board.getPeg[i].getLength() == 3) { //check if there are vertical lines of 3
-                    b1 = Board.getPeg[i].beadlist[0];
-                    b2 = Board.getPeg[i].beadlist[1];
-                    b3 = Board.getPeg[i].beadlist[2];
+            for(int i = 0; i < 16; i++) {
+                if(board.getPeg(i).getLength() == 3) { //check if there are vertical lines of 3
+                    b1 = board.getPeg(i).beadlist[0];
+                    b2 = board.getPeg(i).beadlist[1];
+                    b3 = board.getPeg(i).beadlist[2];
                     
                     if(b1.getColour() == b2.getColour() && b2.getColour() == b3.getColour()) {
                         //check if 3 beads in a line are same colour
-                        row = Board.getPeg[i].getRow();
-                        column = Board.getPeg[i].getColumn();
-                        height = Board.getPeg[i].getLength();
+                        row = board.getPeg(i).getRow();
+                        column = board.getPeg(i).getColumn();
+                        height = board.getPeg(i).getLength();
                     
                         move = new Loc3d(row, column, height);
-                        i = Board.getPeg[].length;
+                        i = 16;
                         //exit loop
                     }
                 }
@@ -118,12 +101,14 @@ public class AI implements ca.unbc.cpsc.score4.interfaces.Player {
         }
         
         turnCount++;
+        lastmove = move;
         return move;
     }
     
     @Override
     public Location retry() throws PlayerException {
-        requestMoveLocation();
+        lastmove.add();
+        return lastmove;
     }
     
     @Override
@@ -138,17 +123,17 @@ public class AI implements ca.unbc.cpsc.score4.interfaces.Player {
     
     private Loc3d randomPlay() {
         //method chooses a peg location at random
-        Loc3d move;
+        Loc3d move = new Loc3d(0,0,0);
         Random random = new Random();
         boolean isLegal = false;
         
         while(!isLegal) {
             int randomIndex = random.nextInt(4);
             
-            if(Board.getPeg[randomIndex].getLength() < 4) { //if peg isn't full
-                int row = Board.getPeg[randomIndex].getRow();
-                int col = Board.getPeg[randomIndex].getColumn();
-                int height = Board.getPeg[randomIndex].getLength();
+            if(board.getPeg(randomIndex).getLength() < 4) { //if peg isn't full
+                int row = board.getPeg(randomIndex).getRow();
+                int col = board.getPeg(randomIndex).getColumn();
+                int height = board.getPeg(randomIndex).getLength();
                 
                 move = new Loc3d(row, col, height);
                 isLegal = true;
