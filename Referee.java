d81a6adba6420f7bf1b8d5366a97e5d8a247fe2f
RyanSilvestre <35552251+RyanSilvestre@users.noreply.github.com>
2018-03-14 12:06:50 -0700

    Add files via upload

--- Referee.java
+++ Referee.java
@@ -22,13 +22,9 @@ public class Referee {
     public void placeBead()
     {
     //communicates with the bead to the peg
-    //isnt this the bead or player class job?
     //checkmoves then places the bead
     }
-    public void displayBoard()
-    {
-    //displays the board
-    }
+
     public boolean checkMove(Location l)
     {
     //communicates with loc3d and loc2d
@@ -50,20 +46,28 @@ public class Referee {
     
     public boolean hasaWin(Loc3d [] aLine)
     {
-        //
-//     int index =0;
-//     switch (index)
-//     {
-//         case 0:
-//             if ((colourAt(aLine[1],aLine[2],aLine[3]))==pc){return true;}
-//             else if ((colourAt(aLine[4],aLine[8],aLine[12])==pc)){return true;}
-//             else if (colourAt(aLine[5],aLine[10],aLine[15])==pc){return true;}
-//             else {return false;}
-//     }
-//           
-     return false;   
+         for (int i = 0;i < 16; i++)
+         {
+             if (board.getPeg(i).getLength()==4)
+             {
+                 b1 = board.getPeg(i).beadlist[0];
+                 b2 = board.getPeg(i).beadlist[1];
+                 b3 = board.getPeg(i).beadlist[2];
+                 b4 = board.getPeg(i).beadlist[3];
+                 if(b1.getColour() == b2.getColour() && b2.getColour() == b3.getColour()==b4.getColour()) 
+                 {
+                     //win
+                 return true;
+                 }
+             }
+             else
+             {
+             return false;
+             }
+            
+         }
     }
-
+    
     public Colour assignColour(Colour c)//or tracks color
     {
         pc = c;
